54828a2fbab669e80a2753ac31182a4aebaf5632
Scott <agentwinburn1@gmail.com>
2020-10-01 20:13:52 +0800

    login, mapview, searchUI, firebase

---
+++ UberApp/Controller/HomeController.swift
@@ -0,0 +1,111 @@
+//
+//  HomeController.swift
+//  UberApp
+//
+//  Created by Scott on 9/28/20.
+//
+
+import UIKit
+import Firebase
+import MapKit
+
+class HomeController: UIViewController {
+    
+    // MARK: - Properties
+    
+    // map view
+    private let mapView = MKMapView()
+    private let locationManager = CLLocationManager()
+    
+    private let inputActivationView = LocationInputActivationView()
+    
+    // MARK: - Lifecycle
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.navigationController?.navigationBar.isHidden = true
+        checkIfUserIsLoggedIn()
+        enableLocationServices()
+        //signOut()
+    }
+    
+    // MARK: - API
+    
+    func checkIfUserIsLoggedIn() {
+        if Auth.auth().currentUser?.uid == nil {
+            
+            DispatchQueue.main.async {
+                let nav = UINavigationController(rootViewController: LoginController())
+                //nav.modalPresentationStyle = .fullScreen
+                self.present(nav, animated: true, completion: nil)
+            }
+            
+            print("User not logged in.")
+            
+        } else {
+            configureUI()
+            print("User is logged in")
+        }
+    }
+    
+    func signOut() {
+        do {
+            try Auth.auth().signOut()
+        } catch {
+            print("Error signing out")
+        }
+    }
+    
+    // MARK: - Helper Functions
+    
+    func configureUI() {
+        configureMapView()
+        
+        view.addSubview(inputActivationView)
+        inputActivationView.centerX(inView: view)
+        inputActivationView.setDimensions(height: 50, width: view.frame.width - 64)
+        inputActivationView.anchor(top: view.safeAreaLayoutGuide.topAnchor, paddingTop: 32)
+    }
+    
+    func configureMapView() {
+        view.addSubview(mapView)
+        mapView.frame = view.frame
+        
+        mapView.showsUserLocation = true
+        mapView.setUserTrackingMode(MKUserTrackingMode.follow, animated: true)
+    }
+
+}
+
+// MARK: - LocationServices
+
+extension HomeController: CLLocationManagerDelegate {
+    
+    func enableLocationServices() {
+        locationManager.delegate = self
+        
+        switch CLLocationManager.authorizationStatus() {
+        case .notDetermined:
+            print("Not determined")
+            locationManager.requestWhenInUseAuthorization()
+        case .restricted, .denied: break
+        case .authorizedAlways:
+            print("Auth always")
+            locationManager.startUpdatingLocation()
+            locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        case .authorizedWhenInUse:
+            print("Auth when in use..")
+            locationManager.requestAlwaysAuthorization()
+        default:
+            break
+        }
+    }
+    
+    func locationManagerDidChangeAuthorization(_ manager: CLLocationManager) {
+        if locationManager.authorizationStatus == .authorizedWhenInUse {
+            locationManager.requestAlwaysAuthorization()
+        }
+    }
+    
+}
